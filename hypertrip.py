546a808e2b5bf9cbaed66ce95db56f64649cb481
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-09-02 17:10:17 +0200

    get_country_data

--- hypertrip.py
+++ hypertrip.py
@@ -1,7 +1,13 @@
-from utils import get_city_soup, get_country_soup
+from utils import get_city_soup, get_country_soup, get_country_data
 
 city = "Seoul"  # input("Please write the name of the city you want to go to: ")
 
 city_soup = get_city_soup(city)
 
 country, country_soup = get_country_soup(city_soup)
+
+print(f'Gathering data for: {city}/{country}')
+
+language, currency, currency_iso, timezone = get_country_data(country_soup)
+
+print(f'Language: {language}, Currency: {currency}, Timezone: {timezone}')
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 
@@ -18,10 +19,21 @@ def get_city_soup(name):
 
 
 def get_country_soup(soup):
-    info_box = soup.select_one(".geography")
+    info_box = soup.select_one("table.geography")
     country_link = info_box.find(text="Country")
     country = country_link.findNext("td").text
     country_request = requests.get(f'{wiki_url}{format_names(country)}')
+    if country_request.status_code == 404:
+        raise Exception("Country Not Found")
     country_soup = BeautifulSoup(country_request.text, "html.parser")
     name = country_soup.find("h1", {'id': "firstHeading"}).text
     return [name, country_soup]
+
+
+def get_country_data(soup):
+    info_box = soup.select_one("table", {'class': "geography"})
+    language = info_box.select_one(".mergedtoprow", text="Official languages").find("td").find("a").text
+    currency = info_box.find(text="Currency").findNext("td")
+    currency_name, currency_iso = currency.findAll("a")
+    timezone = info_box.find(text="Time zone").findNext("td").text
+    return [language, currency_name.text, currency_iso.text, timezone]
